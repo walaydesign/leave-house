e0ac3747dfc7f61a383da961b6f85f3f5ca27655
youzhen <yl301114@gmail.com>
2025-05-09 10:36:58 +0100

    修改1

--- public/js/index.js
+++ public/js/index.js
@@ -49,140 +49,20 @@ $(".architecture_btn button").click(function(){
     swiperArtchitecture.slideTo(li_index, 1000, true);
 })
 
+$(".map_pic_img").click(function() {
+    $(".map-popup").fadeIn(300).css("display","flex");
+})
 
-
-
-
-var swiperSweetlife = new Swiper(".sweetlife_swiper", {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    slidesPerGroup: 1,
-    navigation: {
-        nextEl: ".core_swiper-next",
-        prevEl: ".core_swiper-prev",
-    },
-    speed: 800,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    allowTouchMove: false,
-    breakpoints: {
-        992: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-        }
+$(".map-popup").click(function (event) {
+    var img = $(".map-popup_img");
+    if (!img.is(event.target) && img.has(event.target).length === 0) {
+        $(".map-popup").fadeOut(300);
     }
-})
+});
 
-var swiperSchoolText = new Swiper(".school_swiper-text", {
-    slidesPerView: 1,
-    speed: 800,
-    allowTouchMove: false,
-    navigation: {
-        nextEl: "#school_swiper-next",
-        prevEl: "#school_swiper-prev",
-    },
-    pagination: {
-        el: "#school_swiper-pagination",
-        clickable: true,
-    },
-})
 
-var swiperSchoolPic = new Swiper(".school_swiper-pic", {
-    slidesPerView: 1,
-    speed: 800,
-    navigation: {
-        nextEl: "#school_swiper-next",
-        prevEl: "#school_swiper-prev",
-    },
-    pagination: {
-        el: "#school_swiper-pagination",
-        clickable: true,
-    },
-})
 
-$(".school_tab_item").click(function(){
-    $(this).addClass("active").siblings(".school_tab_item").removeClass("active");
-    let li_index = $(this).index();
-    swiperSchoolPic.slideTo(li_index, 1000, true);
-    swiperSchoolText.slideTo(li_index, 1000, true);
-})
 
-var swiperGreen = new Swiper(".green_swiper", {
-    slidesPerView: 1,
-    speed: 800,
-    navigation: {
-        nextEl: "#green_swiper-next",
-        prevEl: "#green_swiper-prev",
-    },
-    pagination: {
-        el: "#green_swiper-pagination",
-        clickable: true,
-    },
-})
-
-$(".green_tab_item").click(function(){
-    $(this).addClass("active").siblings(".green_tab_item").removeClass("active");
-    let li_index = $(this).index();
-    swiperGreen.slideTo(li_index, 1000, true);
-})
-
-var swiperBuildingPic = new Swiper(".building_swiper-pic", {
-    slidesPerView: 1,
-    speed: 800,
-    allowTouchMove: false,
-    navigation: {
-        nextEl: "#building_swiper_next",
-        prevEl: "#building_swiper_prev",
-    },
-    pagination: {
-        el: "#building_swiper_pagination",
-        clickable: true,
-    },
-})
-
-var swiperBuildingText = new Swiper(".building_swiper-text", {
-    slidesPerView: 1,
-    speed: 800,
-    allowTouchMove: false,
-    navigation: {
-        nextEl: "#building_swiper_next",
-        prevEl: "#building_swiper_prev",
-    },
-    pagination: {
-        el: "#building_swiper_pagination",
-        clickable: true,
-    },
-})
-
-var swiperArrangePic = new Swiper(".arrange_swiper-pic", {
-    slidesPerView: 1,
-    speed: 800,
-    allowTouchMove: false,
-    navigation: {
-        nextEl: "#arrange_swiper_next",
-        prevEl: "#arrange_swiper_prev",
-    },
-    pagination: {
-        el: "#arrange_swiper_pagination",
-        clickable: true,
-    },
-})
-
-var swiperArrangeText = new Swiper(".arrange_swiper-text", {
-    slidesPerView: 1,
-    speed: 800,
-    allowTouchMove: false,
-    navigation: {
-        nextEl: "#arrange_swiper_next",
-        prevEl: "#arrange_swiper_prev",
-    },
-    pagination: {
-        el: "#arrange_swiper_pagination",
-        clickable: true,
-    },
-})
 
 function sendEmail(){
     Email.send({
